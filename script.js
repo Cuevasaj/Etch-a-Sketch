026ec7fcc591759b138b017eba43ca273f378b95
Cuevasaj <92329439+Cuevasaj@users.noreply.github.com>
2025-10-02 17:21:06 -0600

    base game function

--- script.js
+++ script.js
@@ -1,64 +1,58 @@
 // Base game
 let currentSize = 16; // default pixel mode
 let currentMode = "black"; // default color mode
-
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 256; i++) {
-  let pixel = document.createElement("div"); // Element creation
-  pixel.classList.add("pixel");
+const clearContainer = () => {
+  container.replaceChildren();
+};
 
-  pixel.addEventListener("mouseover", () => {
-    pixel.style.backgroundColor = "black"; // styling of element
-  });
+const gridBuild = (size) => {
+  let totalPixels = size * size;
+  container.style.setProperty("--grid-size", size);
+  for (let i = 0; i < totalPixels; i++) {
+    let pixel = document.createElement("div"); // Element creation
+    pixel.classList.add("pixel");
 
-  container.appendChild(pixel);
-}
-// above is the base game
+    pixel.addEventListener("mouseover", () => {
+      pixel.style.backgroundColor = currentMode;
+    });
+    container.appendChild(pixel);
+  }
+};
 
-const gridBuild = () => {};
+// base game
+gridBuild(16);
 
 // Grid generation  prompt
-const gridGenerationBtn = document.querySelector("#grid-generation");
-gridGenerationBtn.addEventListener("click", () => {
-  const input = prompt("pick a number between 1 and 100");
 
-  if (input === null) {
-    // this means user cancelled
-    console.log("User Cancelled Generation");
-    return;
-  }
-  const trimmedPrompt = input.trim(); // will remove any white space to user prompt
-  if (!/^\d+$/.test(trimmedPrompt)) {
-    console.log("Not a Valid Entry: Enter only valid number from 1 to 100");
-    return;
-  }
+// const gridGenerationBtn = document.querySelector("#grid-generation");
+// gridGenerationBtn.addEventListener("click", () => {
+//   const input = prompt("pick a number between 1 and 100");
 
-  // console.log(typeof trimmedPrompt); // shows up - varPrompt is a string it needs to be converted to a number
+//   if (input === null) {
+//     // this means user cancelled
+//     console.log("User Cancelled Generation");
+//     return;
+//   }
+//   const trimmedPrompt = input.trim(); // will remove any white space to user prompt
+//   if (!/^\d+$/.test(trimmedPrompt)) {
+//     console.log("Not a Valid Entry: Enter only valid number from 1 to 100");
+//     return;
+//   }
 
-  //  convert to number
-  const gridSize = Number(trimmedPrompt);
-  // console.log(gridSize);
+//   // console.log(typeof trimmedPrompt); // shows up - varPrompt is a string it needs to be converted to a number
 
-  // range check
+//   //  convert to number
+//   const gridSize = Number(trimmedPrompt);
+//   // console.log(gridSize);
 
-  if (gridSize < 1 || gridSize > 100) {
-    console.log("Out of range: enter 1 - 100");
-    return;
-  }
+//   // range check
 
-  currentSize = gridSize;
+//   if (gridSize < 1 || gridSize > 100) {
+//     console.log("Out of range: enter 1 - 100");
+//     return;
+//   }
 
-  container.replaceChildren();
-});
-
-// Grid generation  prompt ^
-
-//Buttons
-
-// const rainbowBtn = document.querySelector("#mode-rainbow");
-// rainbowBtn.addEventListener("click", () => {
-//   // pixel.style.backgroundColor =
+//   currentSize = gridSize;
 // });
-
-//
